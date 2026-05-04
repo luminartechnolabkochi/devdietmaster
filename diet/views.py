92868cac8c020fac9febea104fc107f73cef4260
sajay <sajaykannan10@gmail.com>
2026-05-04 10:44:58 +0530

    post method impl

--- diet/views.py
+++ diet/views.py
@@ -10,4 +10,29 @@ class DietPlanView(View):
 
         return render(request,"profile.html")
     
+    def post(self,request,*args,**kwargs):
+
+        form_data = request.POST
+
+        height_in_cm = form_data.get("height")
+
+        weight_in_kg = form_data.get("weight")
+
+        gender = form_data.get("gender")
+
+        age = form_data.get("age")
+
+        activitylevel = form_data.get("activitylevel")
+
+        target=form_data.get("target")
+
+        duration=form_data.get("duration")
+
+        print(height_in_cm,weight_in_kg,gender,age,activitylevel,duration,target)
+
+        return render(request,"profile.html")
+
+
+
+
     
