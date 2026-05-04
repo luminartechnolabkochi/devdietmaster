68df2f3f0a84dbc88fb33614e0f20ac3e1a486e7
sajay <sajaykannan10@gmail.com>
2026-05-04 11:26:19 +0530

    tde

--- diet/views.py
+++ diet/views.py
@@ -14,25 +14,47 @@ class DietPlanView(View):
 
         form_data = request.POST
 
-        height_in_cm = form_data.get("height")
+        height_in_cm = int(form_data.get("height"))
 
-        weight_in_kg = form_data.get("weight")
+        weight_in_kg = int(form_data.get("weight"))
 
         gender = form_data.get("gender")
 
-        age = form_data.get("age")
+        age = int(form_data.get("age"))
 
-        activitylevel = form_data.get("activitylevel")
+        activitylevel = float(form_data.get("activitylevel",1.2))
 
-        target=form_data.get("target")
+        target=int(form_data.get("target"))
 
-        duration=form_data.get("duration")
+        duration=int(form_data.get("duration"))
 
         print(height_in_cm,weight_in_kg,gender,age,activitylevel,duration,target)
 
+        tdee=daily_calorie_consumption(gender=gender,weight=weight_in_kg,height=height_in_cm,age=age,activitylevel=activitylevel)
+
+        print("TDEE",tdee)
         return render(request,"profile.html")
 
 
+def daily_calorie_consumption(gender="male",weight=None,height=None,age=None,activitylevel=1.2):
 
+    """
+    
+    """
+    if gender == "male":
+
+        bmr = 10 * weight + 6.25 * height - 5*age + 5
+
+    else:
+
+        bmr = 10 * weight + 6.25 * height - 5*age - 161
+
+    tdee = bmr * activitylevel
+
+    return tdee
 
     
+
+
+
+
